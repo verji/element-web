37b7bed33718721d2eb126bf6fd416f5209a2606
John Tore Simonsen <john.tore.simonsen@rosberg.com>
2024-05-08 15:04:41 +0200

    yarn build

--- scripts/docker-link-module-api.sh
+++ scripts/docker-link-module-api.sh
@@ -5,6 +5,7 @@ git clone --depth 1 --branch $MODULE_API_BRANCH "$MODULE_API_REPO" module-api
 cd module-api
 yarn link
 yarn --network-timeout=100000 install
+yarn build
 cd ../
 
 echo "Setting up element-web with module-api packages"
